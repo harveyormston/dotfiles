d6ec6d32d053cce3371929196d5ab7218a928f5b
Harvey Ormston <htormston@gmail.com>
2017-10-23 20:57:39 +0100

    Add install script.

---
+++ install.sh
@@ -0,0 +1,17 @@
+git submodule init
+git submodule update
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ln -s -f $DIR/.bash_profile ~/.bash_profile
+ln -s -f $DIR/.bashrc ~/.bashrc
+ln -s -f $DIR/.bin ~/.bin
+ln -s -f $DIR/.git-prompt.sh ~/.git-prompt.sh
+ln -s -f $DIR/.ps1-git.sh ~/.ps1-git.sh
+ln -s -f $DIR/.tmux.conf ~/.tmux.conf
+ln -s -f $DIR/.vim ~/.vim
+ln -s -f $DIR/.vimrc ~/.vimrc
+ln -s -f $DIR/.zshrc ~/.zshrc
+ln -s -f $DIR/gitignore_global ~/.gitignore
+ln -s -f $DIR/oh-my-zsh ~/.oh-my-zsh
+ln -s -f $DIR/showsix.zsh-theme ~/.oh-my-zsh/themes/showsix.zsh-theme
